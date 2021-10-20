7e3c2561bf4a9864c2d38074c4191e6a43ab14b2
valliveluvarthi <simhavalli25397@gmail.com>
2021-10-20 21:25:34 +0530

    validations completed

--- src/components/disco/circle-input.js
+++ src/components/disco/circle-input.js
@@ -1,40 +1,47 @@
 import React, { useState } from "react";
 import ColorPicker from "./color-picker";
-import './circle-input.scss';
+import "./circle-input.scss";
 const CircleInput = () => {
-    let [circleInputValue, setCircleInputValue] = useState(1);
-    let [inputHasError, setInputHasError] = useState(false);
-    let [maxInputError, setMaxInputError] = useState(false);
-    const circleInputChangeHandler = (event) => {
-        if(event.target.value > 0){
-            if(event.target.value < 7){
-                setMaxInputError(false);
-                setCircleInputValue(event.target.value);
-            }else{
-                setMaxInputError(true);
-            }
-            setInputHasError(false);
-        }else{
-            setInputHasError(true);
-        }
+  let [circleInputValue, setCircleInputValue] = useState(1);
+  let [inputHasError, setInputHasError] = useState(false);
+  let [maxInputError, setMaxInputError] = useState(false);
+  const circleInputChangeHandler = (event) => {
+    if (event.target.value > 0) {
+      if (event.target.value < 7) {
+        setMaxInputError(false);
+        setCircleInputValue(event.target.value);
+      } else {
+        setMaxInputError(true);
+      }
+      setInputHasError(false);
+    } else {
+      setCircleInputValue(event.target.value);
+      setInputHasError(true);
     }
-    return (
-        <React.Fragment>
-            <div className="circle-form-control">
-            <label htmlFor="circle-number">Enter no.of circles : </label>
-            <input
-                type="number"
-                id="circle-number"
-                value={circleInputValue}
-                onChange={circleInputChangeHandler}
-              />
-            </div>
-            {inputHasError && <p className="circle-error-text">Please enter a positive input value.</p>}
-            {maxInputError && <p className="circle-error-text">Please enter a value less than 7.</p>}
+  };
+  return (
+    <React.Fragment>
+      <div className="circle-form-control">
+        <label htmlFor="circle-number">Enter no.of circles : </label>
+        <input
+          type="number"
+          id="circle-number"
+          value={circleInputValue}
+          onChange={circleInputChangeHandler}
+        />
+      </div>
+      {inputHasError && (
+        <p className="circle-error-text">
+          Please enter a positive input value.
+        </p>
+      )}
+      {maxInputError && (
+        <p className="circle-error-text">Please enter a value less than 7.</p>
+      )}
 
-            {/* color picker component  */}
-            <ColorPicker noOfCircles = {circleInputValue}/>
-        </React.Fragment>
-    );
-}
+      {/* color picker component  */}
+      <ColorPicker noOfCircles={circleInputValue} />
+    </React.Fragment>
+  );
+};
 export default CircleInput;
