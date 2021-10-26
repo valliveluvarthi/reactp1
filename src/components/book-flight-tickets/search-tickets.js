28593cb8f9f20fd3d28fed54d3ac37f27c980182
valliveluvarthi <simhavalli25397@gmail.com>
2021-10-27 00:58:24 +0530

    content equipped for divs in search div

--- src/components/book-flight-tickets/search-tickets.js
+++ src/components/book-flight-tickets/search-tickets.js
@@ -7,6 +7,13 @@ const SearchTickets = () => {
           <span className="heading-primary--main">Its's more than</span>
           <span className="heading-primary--main moveup">just a trip</span>
         </h1>
+        <div className="search-div">
+                <div>div1</div>
+                <div>div2</div>
+                <div>div3</div>
+                <div>div4</div>
+                <div>div5</div>
+        </div>
     </div>
   );
 };
