62958974aab47fa1702e08fcfaed68838b3b9c04
valliveluvarthi <simhavalli25397@gmail.com>
2021-10-27 00:42:49 +0530

    seach text developed

--- src/components/book-flight-tickets/search-tickets.js
+++ src/components/book-flight-tickets/search-tickets.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./search-tickets.scss";
 const SearchTickets = () => {
-    return (
-        <div className="search-tickets-wrapper">
-            search component works!
-        </div>
-    );
-}
+  return (
+    <div className="search-tickets-wrapper">
+        <h1 className="heading-primary">
+          <span className="heading-primary--main">Its's more than</span>
+          <span className="heading-primary--main moveup">just a trip</span>
+        </h1>
+    </div>
+  );
+};
 export default SearchTickets;
