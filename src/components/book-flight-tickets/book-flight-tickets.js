3df22e1ffef23cae5fd0eb4bc036dbff3d6d5759
valliveluvarthi <simhavalli25397@gmail.com>
2021-10-26 18:56:39 +0530

    header added with responsiveness

--- src/components/book-flight-tickets/book-flight-tickets.js
+++ src/components/book-flight-tickets/book-flight-tickets.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
+import Header from './header';
 import "./book-flight-tickets.scss";
 const BookFlightTickets = () => {
     return (
             <div className="book-tickets-wrapper">
-                <p>Book Tickets Working!</p>
+                <Header/>
             </div>
     );
 }
