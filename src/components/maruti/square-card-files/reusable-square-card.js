171c30450ed0e0f61a096cd30d44470e735c80b4
valliveluvarthi <simhavalli25397@gmail.com>
2021-11-10 19:41:00 +0530

    square component completed - alignment left

--- src/components/maruti/square-card-files/reusable-square-card.js
+++ src/components/maruti/square-card-files/reusable-square-card.js
@@ -1,10 +1,81 @@
 import React from "react";
 
 const ReusableSquareCardComponent = (props) => {
-    return(
-        <React.Fragment>
-            <p>Reusable square card Component</p>
-        </React.Fragment>
-    );
-}
+  const onMouseEnter = (event) => {
+    let card_wrapper = document.getElementById(event.currentTarget.id);
+    if (card_wrapper !== null && props.object.url_path_hover) {
+      card_wrapper.src = props.object.url_path_hover;
+    }
+  };
+  const onMouseLeave = (event) => {
+    let card_wrapper = document.getElementById(event.currentTarget.id);
+    if (card_wrapper !== null && props.object.url_path) {
+      card_wrapper.src = props.object.url_path;
+    }
+  };
+  const mouseOnContent = (event) => {
+    let backgrnd_img_id = "square-card-" + event.currentTarget.id;
+    let background_img_elemt = document.getElementById(backgrnd_img_id);
+    if (background_img_elemt !== null && props.object.url_path_hover) {
+      background_img_elemt.src = props.object.url_path_hover;
+    }
+  };
+  const mouseExitContent = (event) => {
+    let backgrnd_img_id = "square-card-" + event.currentTarget.id;
+    let background_img_elemt = document.getElementById(backgrnd_img_id);
+    if (background_img_elemt !== null && props.object.url_path) {
+      background_img_elemt.src = props.object.url_path;
+    }
+  };
+  return (
+    <div className="sq-card-wrapper">
+      <img
+        id={props.object.id}
+        className="background-image"
+        src={props.object.url_path}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      />
+      <div
+        className="sq-card-content"
+        id={props.object.s_no}
+        onMouseEnter={mouseOnContent}
+        onMouseLeave={mouseExitContent}
+      >
+        <div className="sq-card-title poppins-title-24">
+          {props.object.title}
+        </div>
+        {props.object.knowmore_text && (
+          <div className="know-more">
+            <div className="know-more-red-text">
+              <img
+                className="image-text"
+                src={props.object.know_more_red_text_svg}
+                alt="know more 1"
+              />
+              <img
+                className="image-arrow"
+                src={props.object.know_more_red_arrow_svg}
+                alt="know more 1"
+              />
+            </div>
+            <div className="know-more-white-text">
+              <img
+                className="image-text"
+                src={props.object.know_more_white_text_svg}
+                alt="know more 2"
+              />
+              <img
+                className="image-arrow"
+                src={props.object.know_more_white_arrow_svg}
+                alt="know more 2"
+              />
+            </div>
+            <div className="know-more-text-decoration"></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 export default ReusableSquareCardComponent;
--- src/components/maruti/square-card-files/square-card.js
+++ src/components/maruti/square-card-files/square-card.js
@@ -1,8 +1,105 @@
 import React from "react";
-import ReusableSquareCardComponent from './reusable-square-card';
-const SquareCardComponent = (props) => {
-    return(
-        <ReusableSquareCardComponent/>
+import ReusableSquareCardComponent from "./reusable-square-card";
+import "./reusable-square-card.scss";
+
+class SquareCardComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product_e_cards: [
+        {
+          s_no: "1",
+          id: "square-card-1",
+          url_path:
+            "https://cdn-gcp.marutitech.com/wp-content/uploads/2019/05/card-1.svg",
+          url_path_hover:
+            "https://cdn-gcp.marutitech.com/wp-media/2019/05/card-1-Copy.svg",
+          title: "Cloud Application",
+          knowmore_text: true,
+          know_more_red_text_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-red-text.svg",
+          know_more_red_arrow_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-red-arrow.svg",
+          know_more_white_text_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-white-text.svg",
+          know_more_white_arrow_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-white-arrow.svg",
+        },
+        {
+          s_no: "2",
+          id: "square-card-2",
+          url_path:
+            "https://cdn-gcp.marutitech.com/wp-content/uploads/2019/05/card-2.svg",
+          url_path_hover:
+            "https://cdn-gcp.marutitech.com/wp-content/uploads/2019/05/card-2-Copy.svg",
+          title: "DevOps",
+          knowmore_text: true,
+          know_more_red_text_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-red-text.svg",
+          know_more_red_arrow_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-red-arrow.svg",
+          know_more_white_text_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-white-text.svg",
+          know_more_white_arrow_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-white-arrow.svg",
+        },
+        {
+          s_no: "3",
+          id: "square-card-3",
+          url_path:
+            "https://cdn-gcp.marutitech.com/wp-media/2019/05/card-3.svg",
+          url_path_hover:
+            "https://cdn-gcp.marutitech.com/wp-content/uploads/2019/05/card-3-Copy.svg",
+          title: "Quality Engineering",
+          knowmore_text: true,
+          know_more_red_text_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-red-text.svg",
+          know_more_red_arrow_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-red-arrow.svg",
+          know_more_white_text_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-white-text.svg",
+          know_more_white_arrow_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-white-arrow.svg",
+        },
+        {
+          s_no: "4",
+          id: "square-card-4",
+          url_path:
+            "https://cdn-gcp.marutitech.com/wp-content/uploads/2019/05/card-4.svg",
+          url_path_hover:
+            "https://cdn-gcp.marutitech.com/wp-content/uploads/2019/05/card-4-Copy.svg",
+          title: "Rapid Prototyping",
+          knowmore_text: true,
+          know_more_red_text_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-red-text.svg",
+          know_more_red_arrow_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-red-arrow.svg",
+          know_more_white_text_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-white-text.svg",
+          know_more_white_arrow_svg:
+            "https://marutitech.com/wp-content/uploads/2019/05/know-more-white-arrow.svg",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="sq-packed-div">
+          {this.state.product_e_cards.length > 0 &&
+            this.state.product_e_cards.map((item, i) => {
+              return (
+                <ReusableSquareCardComponent
+                  key={i}
+                  object={item}
+                  className={item.id}
+                />
+              );
+            })}
+        </div>
+      </React.Fragment>
     );
+  }
 }
+
 export default SquareCardComponent;
