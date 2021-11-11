3a4d32256a53612d98efe5efdcac318360ce395a
valliveluvarthi <simhavalli25397@gmail.com>
2021-11-11 19:01:01 +0530

    image driven component witout responsiveness

--- src/components/maruti/image-comp-files/image-comp.js
+++ src/components/maruti/image-comp-files/image-comp.js
@@ -1,8 +1,109 @@
 import React from "react";
-import ReusableImageComponent from './reusable-image-comp';
-const ImageComponent = (props) => {
-    return(
-        <ReusableImageComponent/>
+import ReusableImageComponent from "./reusable-image-comp";
+import "./reusable-image-comp.scss";
+class ImageComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image_driven_comp: [
+        {
+          type: "desc_driven_comp",
+          title: "WotNot",
+          background : "#ffffff",background : "#ffffff",
+          description:
+            "WotNot is a leading chatbot development platform that helps you scale how you sell, market or support with custom chatbots. Address manual tasks & allow demand generation or support teams to do what they do best.",
+          url_path:
+            "https://cdn-gcp.marutitech.com/wp-media/2019/10/80c48941-wotnot-hero-compressor.png",
+          learn_more: true,
+          advantages: [
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/06/MT__Increased-ROIs.svg",
+              img_desc: "Increased ROIs",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/05/MT_Platform-agnostic-Ai-flow.svg",
+              img_desc: "Platform-agnostic AI Flow",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/06/Lead-generation-v1.svg",
+              img_desc: "Lead Generation",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/06/MT__Always-on.svg",
+              img_desc: "Always On - 24/7",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/11/MT_Multilingual-fluency.svg",
+              img_desc: "Multilingual Fluency",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/06/MT__Personalized-Interaction.svg",
+              img_desc: "Personalized Interactions",
+            },
+          ],
+        },
+        {
+          type: "img_driven_comp",
+          title: "Alertly",
+          background : "#383838",
+          description:
+            "Use intelligent automation to automate your JIRA alerts for Slack. If you use JIRA for project management, integrate Alertly to get status updates & carry out asynchronous stand up meetings - without leaving Slack!",
+          url_path:
+            "https://cdn-gcp.marutitech.com/wp-media/2019/10/05512317-alertly-hero-img-compressor.png",
+          learn_more: true,
+          advantages: [
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/11/MT_manage-time-and-pace.svg",
+              img_desc: "Manage Time and Pace",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/11/MT_Custom-alerts-for-one-and-all.svg",
+              img_desc: "Custom alerts for one and all",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/11/MT_Actionable-alerts.svg",
+              img_desc: "Actionable Alerts",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/11/MT_Track-Milestone.svg",
+              img_desc: "Track Milestones",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/11/MT_Project-overview-and-visibility.svg",
+              img_desc: "Marketplace Synchronization",
+            },
+            {
+              img_url:
+                "https://cdn-gcp.marutitech.com/wp-media/2019/11/MT_Team-collaboration.svg",
+              img_desc: "Easy Team Management",
+            },
+          ],
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="img-packed-div">
+          {this.state.image_driven_comp.length > 0 &&
+            this.state.image_driven_comp.map((item, i) => {
+              return <ReusableImageComponent key={i} object={item} />;
+            })}
+        </div>
+      </React.Fragment>
     );
+  }
 }
 export default ImageComponent;
--- src/components/maruti/image-comp-files/reusable-image-comp.js
+++ src/components/maruti/image-comp-files/reusable-image-comp.js
@@ -1,10 +1,74 @@
 import React from "react";
 
 const ReusableImageComponent = (props) => {
-    return(
-        <React.Fragment>
-            <p>Reusable image Component</p>
-        </React.Fragment>
+  let element;
+  if (props.object.type === "desc_driven_comp") {
+    element = (
+      <div
+        className="img-comp-wrapper desc-driven-wrapper"
+        style={{
+          backgroundColor: props.object.background,
+        }}
+      >
+        <div className="desc-driven-title">{props.object.title}</div>
+        <div className="desc-driven-desc">{props.object.description}</div>
+        <div className="desc-advantages">
+          {props.object.advantages.length > 0 &&
+            props.object.advantages.map((item, i) => {
+              return (
+                <React.Fragment key={i}>
+                  <img
+                    className="adv-img"
+                    src={item.img_url}
+                    alt={item.img_desc}
+                  />
+                  <div className="adv-desc">{item.img_desc}</div>
+                </React.Fragment>
+              );
+            })}
+        </div>
+        <img
+          className="desc-driven-img"
+          src={props.object.url_path}
+          alt="Wotnot Img"
+        ></img>
+        <div className="desc-learn-more-btn btn-content">Learn More</div>
+      </div>
     );
-}
+  } else if (props.object.type === "img_driven_comp") {
+    element = (
+      <div
+        className="img-comp-wrapper img-driven-wrapper"
+        style={{
+          backgroundColor: props.object.background,
+        }}
+      >
+        <div className="img-driven-title">{props.object.title}</div>
+        <div className="img-driven-desc">{props.object.description}</div>
+        <div className="img-advantages">
+          {props.object.advantages.length > 0 &&
+            props.object.advantages.map((item, i) => {
+              return (
+                <React.Fragment key={i}>
+                  <img
+                    className="adv-img"
+                    src={item.img_url}
+                    alt={item.img_desc}
+                  />
+                  <div className="adv-desc">{item.img_desc}</div>
+                </React.Fragment>
+              );
+            })}
+        </div>
+        <img
+          className="img-driven-img"
+          src={props.object.url_path}
+          alt="Wotnot Img"
+        ></img>
+        <div className="img-learn-more-btn btn-content">Learn More</div>
+      </div>
+    );
+  }
+  return element;
+};
 export default ReusableImageComponent;
